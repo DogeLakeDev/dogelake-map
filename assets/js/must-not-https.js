d35add6016145c4279a28f9a0aa4c2bf3108b7b4
Cdm2883 <Cdm2883@hotmail.com>
2024-02-03 23:57:21 +0800

    add a feature that prohibits access using HTTPS

---
+++ assets/js/must-not-https.js
@@ -0,0 +1,5 @@
+(() => {
+    if (window.location.protocol === 'https:' || window.location.protocol === 'https') {
+        window.location.href = window.location.href.replace(/^https/, 'http');
+    }
+})()
