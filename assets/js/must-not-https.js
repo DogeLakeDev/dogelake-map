0a2f37c213d5190dae69ee5858739e7986db38ba
Cdm2883 <Cdm2883@hotmail.com>
2024-02-05 16:05:04 +0800

    change must-not-https.js for bette

--- assets/js/must-not-https.js
+++ assets/js/must-not-https.js
@@ -1,5 +1,6 @@
 (() => {
     if (window.location.protocol === 'https:' || window.location.protocol === 'https') {
+        alert('该网页无法在https协议下正常浏览, 正在尝试跳转到http!');
         window.location.href = window.location.href.replace(/^https/, 'http');
     }
 })()
